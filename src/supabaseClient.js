756bb4b22e54a8b855bc0dd4b3f09c2a23006739
박태희 <taehee@bagtaehuiui-MacBookPro.local>
2026-06-19 13:36:49 +0900

    supabase anon key 수정

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-
+const supabaseUrl = 'https://nvfzarmegyakzzmodcln.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52Znphcm1lZ3lha3p6bW9kY2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTk4MTUsImV4cCI6MjA5NzIzNTgxNX0.cG0sdYNCWSgVP6LYrhfsxrKnwlLwO3C48L7ihWrbqIg'
 export const supabase = createClient(supabaseUrl, supabaseKey)
